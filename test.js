9aeed9a9b9fbe977a3b87bf9c833d6b68666b74b
David Avraamides <david.avraamides@gmail.com>
2019-05-24 17:24:23 -0400

    remove test files

--- test.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs");
-var re = /^#+\s+Tasks\s*\n(.*?)\n\n/sm;
-var txt = fs.readFileSync('todo.txt');
-var s = re.exec(txt.toString());
-console.log(s[1]);
--- test.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs';
-let re = /^#+\s+Tasks\s*\n(.*?)\n\n/m;
-let txt = fs.readFileSync('todo.txt');
-let s = re.exec(txt.toString());
-console.log(s);
